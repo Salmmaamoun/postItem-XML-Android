d727bdcfcf8bee6c4ecc0e4251119729bdf493d0
salmmaamoun <allamsalma299@gmail.com>
2022-12-15 18:35:59 +0200

    first

---
+++ app/src/main/java/com/example/postitem_route/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.postitem_route;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
